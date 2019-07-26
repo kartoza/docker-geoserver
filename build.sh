a0618c4df9c0548f84d4f0d05dc11d73c2d0dab7
mazano <kartozanotifications@gmail.com>
2019-07-26 13:45:18 +0200

    Fix various issues in the repo (#80)
    * removed tomcat apr and native install since tomcat base installs this
    
    * add variable for resource directory
    
    * modified wget requests
    
    * upgrade Geoserver to version 2.15.0
    
    * Develop (#65)
    
    * Develop (#63)
    
    * removed tomcat apr and native install since tomcat base installs this
    
    * add variable for resource directory
    
    * modified wget requests
    
    * upgrade Geoserver to version 2.15.0
    
    * Develop (#64)
    
    * removed tomcat apr and native install since tomcat base installs this
    
    * add variable for resource directory
    
    * modified wget requests
    
    * upgrade Geoserver to version 2.15.0
    
    * Fix gdal-data unzip and using latest postgis in docker compose (#66)
    
    * Use postgres 11 and remove quotes from geoserver env password
    
    * fix gdal-data unzip
    
    * Add option for env variable for custom location of S3 properties file
    
    * Fix bound mounts and add S3 env variable
    
    * Changed exit code for update password script
    
    * Modified scripts to solve loggin issues

--- build.sh
+++ build.sh
@@ -1,32 +1,24 @@
 #!/bin/sh
-# Check the GeoServer version specified in the Dockerfile and substitute the number in the globals starting with OLD.
 
-# This represents the version we need GeoServer to move up to. ie the latest stable version.
-# Change this to reflect changes
-BUGFIX=0
-MINOR=14
+BUGFIX=2
+MINOR=15
 MAJOR=2
-
-#Represents the current version defined in the Dockerfile and used in the setup script
-OLD_MAJOR=2
-OLD_MINOR=13
-OLD_BUGFIX=0
 
 # Build Geoserver
 echo "Building GeoServer using the specified version "
-sed -i "s/${OLD_MAJOR}.${OLD_MINOR}.${OLD_BUGFIX}/${MAJOR}.${MINOR}.${BUGFIX}/g" Dockerfile
+
 docker build --build-arg GS_VERSION=${MAJOR}.${MINOR}.${BUGFIX} -t kartoza/geoserver:${MAJOR}.${MINOR}.${BUGFIX} .
 
 # Build Arguments - To change the defaults when building the image
 #need to specify a different value.
-```
---build-arg ORACLE_JDK=true
---build-arg COMMUNITY_MODULES=true
---build-arg TOMCAT_EXTRAS=false
---build-arg WAR_URL=http://downloads.sourceforge.net/project/geoserver/GeoServer/<GS_VERSION>/geoserver-<GS_VERSION>-war.zip
---build-arg INITIAL_MEMORY=2G
---build-arg MAXIMUM_MEMORY=4G
-```
+
+#--build-arg ORACLE_JDK=true
+#--build-arg COMMUNITY_MODULES=true
+#--build-arg TOMCAT_EXTRAS=false
+#--build-arg WAR_URL=http://downloads.sourceforge.net/project/geoserver/GeoServer/<GS_VERSION>/geoserver-<GS_VERSION>-war.zip
+#--build-arg INITIAL_MEMORY=2G
+#--build-arg MAXIMUM_MEMORY=4G
+
 
 
 
