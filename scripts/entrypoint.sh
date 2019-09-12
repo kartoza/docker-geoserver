f22b535a4c4221611039fc3df37b72d276a16529
mazano <kartozanotifications@gmail.com>
2019-09-12 16:19:25 +0200

    Marling render (#100)
    * removed tomcat apr and native install since tomcat base installs this
    
    * add variable for resource directory
    
    * modified wget requests
    
    * upgrade Geoserver to version 2.15.0
    
    * Develop (#65)
    
    * Develop (#63)
    
    * removed tomcat apr and native install since tomcat base installs this
    
    * add variable for resource directory
    
    * modified wget requests
    
    * upgrade Geoserver to version 2.15.0
    
    * Develop (#64)
    
    * removed tomcat apr and native install since tomcat base installs this
    
    * add variable for resource directory
    
    * modified wget requests
    
    * upgrade Geoserver to version 2.15.0
    
    * Fix gdal-data unzip and using latest postgis in docker compose (#66)
    
    * Use postgres 11 and remove quotes from geoserver env password
    
    * fix gdal-data unzip
    
    * Add option for env variable for custom location of S3 properties file
    
    * Fix bound mounts and add S3 env variable
    
    * Changed exit code for update password script
    
    * Modified scripts to solve loggin issues
    
    * fix postgis store issue caused by conflicting jar file from backup and restore plugin
    
    * fix import of gdal plugin
    
    * Add instruction to add support for CORS
    
    * added env variables for java startup options
    
    * Enable marlin renderer

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -7,7 +7,10 @@ export GEOSERVER_OPTS="-Djava.awt.headless=true -server -Xms${INITIAL_MEMORY} -X
        -Dorg.geotools.referencing.forceXY=true -XX:SoftRefLRUPolicyMSPerMB=36000 -XX:+UseParallelGC -XX:NewRatio=2 \
        -XX:+CMSClassUnloadingEnabled -Dfile.encoding=UTF8 -Duser.timezone=GMT -Djavax.servlet.request.encoding=UTF-8 \
        -Djavax.servlet.response.encoding=UTF-8 -Duser.timezone=GMT -Dorg.geotools.shapefile.datetime=true \
-       -Dorg.geotools.shapefile.datetime=true -Ds3.properties.location=/opt/geoserver/data_dir/s3.properties "
+       -Dorg.geotools.shapefile.datetime=true -Ds3.properties.location=/opt/geoserver/data_dir/s3.properties \
+       -Xbootclasspath/a:${CATALINA_HOME}/webapps/geoserver/WEB-INF/lib/marlin.jar \
+       -Xbootclasspath/p:${CATALINA_HOME}/webapps/geoserver/WEB-INF/lib/marlin-sun-java2d.jar \
+       -Dsun.java2d.renderer=org.marlin.pisces.PiscesRenderingEngine"
 
 ## Preparare the JVM command line arguments
 export JAVA_OPTS="${JAVA_OPTS} ${GEOSERVER_OPTS}"
