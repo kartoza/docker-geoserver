ea8d1d7e472b5e0d16a8b922c65ca222afbd85a5
admire <admire@kartoza.com>
2021-01-31 22:21:43 +0200

    WIP

--- scripts/update_passwords.sh
+++ scripts/update_passwords.sh
@@ -37,7 +37,26 @@ if [ ! -d "${GEOSERVER_DATA_DIR}/security" ]; then
   cp -r ${CATALINA_HOME}/security ${GEOSERVER_DATA_DIR}
 fi
 
+function file_env {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
 
+file_env 'GEOSERVER_ADMIN_USER'
+file_env 'GEOSERVER_ADMIN_PASSWORD'
 
 
 # Set random password if none provided
