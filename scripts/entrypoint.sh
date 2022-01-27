53e3930b38ff8ece63529fa80ae5f683c779146b
admire <admire@kartoza.com>
2022-01-27 10:42:32 +0200

    added new env variable to allow further customisation of entrypoint - JVM startup options

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -50,7 +50,8 @@ export GEOSERVER_OPTS="-Djava.awt.headless=true -server -Xms${INITIAL_MEMORY} -X
        -Dgeoserver.login.autocomplete=${LOGIN_STATUS} \
        -DGEOSERVER_CONSOLE_DISABLED=${DISABLE_WEB_INTERFACE} \
        -DGEOSERVER_CSRF_WHITELIST=${CSRF_WHITELIST} \
-       -Dgeoserver.xframe.shouldSetPolicy=${XFRAME_OPTIONS} "
+       -Dgeoserver.xframe.shouldSetPolicy=${XFRAME_OPTIONS} \
+       ${ADDITIONAL_JAVA_STARTUP_OPTIONS} "
 
 ## Prepare the JVM command line arguments
 export JAVA_OPTS="${JAVA_OPTS} ${GEOSERVER_OPTS}"
