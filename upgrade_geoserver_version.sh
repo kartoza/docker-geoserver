25ef85e1386c4d6cc276d7bb38c7d15b77d76b9b
Tek Kshetri <39838116+iamtekson@users.noreply.github.com>
2022-04-25 17:21:13 +0700

    correction on sed command

--- upgrade_geoserver_version.sh
+++ upgrade_geoserver_version.sh
@@ -12,11 +12,11 @@ if [  -f "${GS_NEW_VERSION}" ]; then
     rm "${GS_NEW_VERSION}"
 fi
 
-sed -i "" "s/${GS_VERSION}/${GS_NEW_VERSION}/g" "Dockerfile"
+sed -i "s/${GS_VERSION}/${GS_NEW_VERSION}/g" "Dockerfile"
 
-sed -i "" "s/${GS_VERSION}/${GS_NEW_VERSION}/g" "README.md"
+sed -i "s/${GS_VERSION}/${GS_NEW_VERSION}/g" "README.md"
 
-sed -i "" "s/${GS_VERSION}/${GS_NEW_VERSION}/g" ".env"
+sed -i "s/${GS_VERSION}/${GS_NEW_VERSION}/g" ".env"
 
 git commit -a -m "Upgraded GeoServer from version ${GS_VERSION} to ${GS_NEW_VERSION}"
 
