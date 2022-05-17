f40770a5bbb4f29dc0d107a05aafb5f0da09164a
mazano <addloe@gmail.com>
2022-05-17 17:30:05 +0200

    Fix missing variable (#404)
    * fix missing variable and sed command

--- upgrade_geoserver_version.sh
+++ upgrade_geoserver_version.sh
@@ -18,7 +18,7 @@ sed -i "s/${GS_VERSION}/${GS_NEW_VERSION}/g" "README.md"
 
 sed -i "s/${GS_VERSION}/${GS_NEW_VERSION}/g" ".env"
 
-sed -i "" "s/${GS_VERSION}/${GS_NEW_VERSION}/g" "clustering/docker-compose.yml"
+sed -i  "s/${GS_VERSION}/${GS_NEW_VERSION}/g" "clustering/docker-compose.yml"
 
 git commit -a -m "Upgraded GeoServer from version ${GS_VERSION} to ${GS_NEW_VERSION}"
 
