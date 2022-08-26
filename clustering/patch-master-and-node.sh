243be1e1889887ef150a7e5d79ce2af6f59e7816
afabiani <alessio.fabiani@geosolutionsgroup.com>
2022-08-26 12:52:22 +0200

    [Clustering] Downgrade to GeoServer version 2.20.2

--- clustering/patch-master-and-node.sh
+++ clustering/patch-master-and-node.sh
@@ -1,8 +1,8 @@
-docker cp patched_jars/gs-jms-commons-2.21-SNAPSHOT.jar clustering-master-1:/usr/local/tomcat/webapps/geoserver/WEB-INF/lib/gs-jms-commons-2.21-SNAPSHOT.jar
-docker cp patched_jars/gs-jms-commons-2.21-SNAPSHOT.jar clustering-node-1:/usr/local/tomcat/webapps/geoserver/WEB-INF/lib/gs-jms-commons-2.21-SNAPSHOT.jar
+docker cp patched_jars/gs-jms-commons-2.20-SNAPSHOT.jar clustering-master-1:/usr/local/tomcat/webapps/geoserver/WEB-INF/lib/gs-jms-commons-2.20-SNAPSHOT.jar
+docker cp patched_jars/gs-jms-commons-2.20-SNAPSHOT.jar clustering-node-1:/usr/local/tomcat/webapps/geoserver/WEB-INF/lib/gs-jms-commons-2.20-SNAPSHOT.jar
 
-docker cp patched_jars/gs-jms-geoserver-2.21-SNAPSHOT.jar clustering-master-1:/usr/local/tomcat/webapps/geoserver/WEB-INF/lib/gs-jms-geoserver-2.21-SNAPSHOT.jar
-docker cp patched_jars/gs-jms-geoserver-2.21-SNAPSHOT.jar clustering-node-1:/usr/local/tomcat/webapps/geoserver/WEB-INF/lib/gs-jms-geoserver-2.21-SNAPSHOT.jar
+docker cp patched_jars/gs-jms-geoserver-2.20-SNAPSHOT.jar clustering-master-1:/usr/local/tomcat/webapps/geoserver/WEB-INF/lib/gs-jms-geoserver-2.20-SNAPSHOT.jar
+docker cp patched_jars/gs-jms-geoserver-2.20-SNAPSHOT.jar clustering-node-1:/usr/local/tomcat/webapps/geoserver/WEB-INF/lib/gs-jms-geoserver-2.20-SNAPSHOT.jar
 
 docker-compose stop master node
 docker-compose restart master node
