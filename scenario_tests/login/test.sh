a05d3eef39d7474529895ab05456295dc75a8d41
Admire Nyakudya <addloe@gmail.com>
2023-02-28 21:20:14 +0200

    add correct tests

--- scenario_tests/login/test.sh
+++ scenario_tests/login/test.sh
@@ -11,27 +11,25 @@ if [[ $(dpkg -l | grep "docker-compose") > /dev/null ]];then
   else
     VERSION='docker compose'
 fi
-
-${VERSION} up -d
+echo $docker compose
+docker compose up -d
 
 if [[ -n "${PRINT_TEST_LOGS}" ]]; then
-  ${VERSION} logs -f &
+  docker compose logs -f &
 fi
 
 sleep 30
 
-services=("pg" "pg-new" "pg-gosu" "pg-new-gosu")
+
+services=("geoserver")
 
 for service in "${services[@]}"; do
 
   # Execute tests
-  until ${VERSION} exec -T $service pg_isready; do
-    sleep 30
-    echo "Wait service to be ready"
-  done;
+  sleep 60
   echo "Execute test for $service"
-  ${VERSION} exec -T $service /bin/bash /tests/test.sh
+  docker compose exec -T $service /bin/bash /tests/test.sh
 
 done
 
-${VERSION} down -v
+docker compose down -v
