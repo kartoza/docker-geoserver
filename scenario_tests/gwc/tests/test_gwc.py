2439b780dc4d72748675c1a98acb5f462c4ee80c
mazano <addloe@gmail.com>
2023-03-05 18:43:10 +0200

    update tests and build script (#504)
    Add tests and fix compose file

---
+++ scenario_tests/gwc/tests/test_gwc.py
@@ -0,0 +1,47 @@
+import time
+import unittest
+from os import environ
+
+from requests import get, post
+from requests.auth import HTTPBasicAuth
+
+
+class TestGeoServerGWC(unittest.TestCase):
+
+    def setUp(self):
+        # Define the GeoServer URL
+        self.gs_url = 'http://localhost:8080/geoserver'
+        self.store_name = 'states_shapefile'
+        self.geo_username = environ.get('GEOSERVER_ADMIN_USER', 'admin')
+        self.geo_password = environ.get('GEOSERVER_ADMIN_PASSWORD', 'myawesomegeoserver')
+        self.geo_workspace_name = 'topp'
+
+    def test_seed_vector_layer(self):
+        auth = HTTPBasicAuth('%s' % self.geo_username, '%s' % self.geo_password)
+
+        # Seed the vector layer
+
+        layer_name = '%s:states' % self.geo_workspace_name
+        gwc_url = f'{self.gs_url}/gwc/rest/seed/%s.xml' % layer_name
+
+        # Set the GWC seed request parameters
+        headers = {'Content-type': 'text/xml'}
+        data = '<seedRequest><name>%s</name><srs><number>4326</number></srs><zoomStart>1</zoomStart>' \
+               '<zoomStop>6</zoomStop><format>image/png</format><type>seed</type><threadCount>2</threadCount>' \
+               '<parameterFilters>cql_filter:STATE_ABBR=\'TX\'</parameterFilters></seedRequest>' % layer_name
+
+        post(gwc_url, headers=headers, data=data, auth=auth)
+
+        time.sleep(30)
+        wms_request = f'{self.gs_url}/ows?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&BBOX=31.31110495617180689,\
+        -101.7342329829540404,32.41516971387711976,-99.82891832338629001&CRS=EPSG:4326&WIDTH=1564&HEIGHT=906\
+        &LAYERS=%s&STYLES=&FORMAT=image/png&DPI=96 &TILED=true \
+        &MAP_RESOLUTION=96&FORMAT_OPTIONS=dpi:96&TRANSPARENT=TRUE' % layer_name
+        response = get(wms_request)
+
+        # Verify that the seeding was successful
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ scenario_tests/stores/tests/test_jndi.py
@@ -0,0 +1,94 @@
+import unittest
+from os import environ
+
+from geo.Geoserver import Geoserver
+from requests import get, post, exceptions, delete
+from requests.auth import HTTPBasicAuth
+from subprocess import check_call
+
+
+class TestGeoServerJNDI(unittest.TestCase):
+
+    def setUp(self):
+        # Define the GeoServer URL
+        self.gs_url = 'http://localhost:8080/geoserver'
+        # Define the PostGIS JNDI store name
+        self.jndi_store_name = 'gis'
+        self.geo_username = environ.get('GEOSERVER_ADMIN_USER', 'admin')
+        self.geo_password = environ.get('GEOSERVER_ADMIN_PASSWORD', 'myawesomegeoserver')
+        self.geo_workspace_name = 'demo'
+
+    def test_publish_jndi_store(self):
+
+        # Import shp into DB
+        db_importer = '''ogr2ogr -progress -append -skipfailures -a_srs "EPSG:4326" -nlt PROMOTE_TO_MULTI \
+                -f "PostgreSQL" PG:"dbname=gis port=5432 user=docker password=docker host=db" \
+                 /usr/local/tomcat/data/data/shapefiles/states.shp '''
+        check_call(db_importer, shell=True)
+
+        geo = Geoserver(self.gs_url, username='%s' % self.geo_username, password='%s' % self.geo_password)
+
+        auth = HTTPBasicAuth('%s' % self.geo_username, '%s' % self.geo_password)
+        # create workspace
+        try:
+            rest_url = '%s/rest/workspaces/%s.json' % (self.gs_url, self.geo_workspace_name)
+            response = get(rest_url, auth=auth)
+            response.raise_for_status()
+        except exceptions.HTTPError:
+            geo.create_workspace(workspace='%s' % self.geo_workspace_name)
+            geo.set_default_workspace('%s' % self.geo_workspace_name)
+
+        # Create the XML payload for the JNDI store configuration
+        xml = """
+        <dataStore>
+          <name>{name}</name>
+          <type>PostGIS (JNDI)</type>
+          <enabled>true</enabled>
+          <connectionParameters>
+            <entry key="schema">public</entry>
+            <entry key="Estimated extends">true</entry>
+            <entry key="fetch size">1000</entry>
+            <entry key="encode functions">true</entry>
+            <entry key="Expose primary keys">false</entry>
+            <entry key="Support on the fly geometry simplification">true</entry>
+            <entry key="Batch insert size">1</entry>
+            <entry key="preparedStatements">false</entry>
+            <entry key="Method used to simplify geometries">FAST</entry>
+            <entry key="jndiReferenceName">java:comp/env/jdbc/postgres</entry>
+            <entry key="dbtype">postgis</entry>
+            <entry key="Loose bbox">true</entry>
+          </connectionParameters>
+          <disableOnConnFailure>false</disableOnConnFailure>
+        </dataStore>
+        """.format(name=self.jndi_store_name)
+
+        # Publish the JNDI store
+        response = post(self.gs_url + '/rest/workspaces/%s/datastores' % self.geo_workspace_name, auth=auth,
+                        headers={'Content-type': 'text/xml'},
+                        data=xml)
+        # TODO publish a layer into the db and then in geoserver, then test
+
+        # Publish layer into geoserver
+        try:
+            rest_url = '%s/rest/workspaces/%s/datastores/%s/featuretypes/states.json' % (
+                self.gs_url, self.geo_workspace_name, self.jndi_store_name)
+            publish_response = get(rest_url, auth=auth)
+            publish_response.raise_for_status()
+        except exceptions.HTTPError:
+            geo.publish_featurestore(workspace='%s'
+                                               % self.geo_workspace_name, store_name='%s' % self.jndi_store_name,
+                                     pg_table='states')
+        # Check that the response has a status code of 201 (Created)
+        self.assertEqual(response.status_code, 201)
+
+        # Check that the JNDI store exists
+        data_source_url = '%s/rest/workspaces/%s/datastores/%s.json' % (
+            self.gs_url, self.geo_workspace_name, self.jndi_store_name)
+        response = get(data_source_url, auth=auth)
+
+        # Check that the response has a status code of 200 (OK)
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
