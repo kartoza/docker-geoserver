35ae954e189220fc952665d919554788d5013f5b
Alex Kuretz <alex@andersonopt.com>
2025-04-15 13:47:06 -0600

    Upgrade GeoServer version to 2.27.0

--- upgrade_geoserver_version.sh
+++ upgrade_geoserver_version.sh
@@ -22,9 +22,9 @@ sed -i  "s/${GS_VERSION}/${GS_NEW_VERSION}/g" "clustering/docker-compose.yml"
 
 IFS='.' read -r MAJOR MINOR BUGFIX <<<"$GS_NEW_VERSION"
 
-sed -i '' "s/minor: .*/minor: $MINOR/g; s/patch: .*/patch: $BUGFIX/g" .github/workflows/deploy-image.yaml
+sed -i "s/minor: .*/minor: $MINOR/g; s/patch: .*/patch: $BUGFIX/g" ".github/workflows/deploy-image.yaml"
 
-sed -i '' "s/minor: .*/minor: $MINOR/g; s/patch: .*/patch: $BUGFIX/g" .github/workflows//build-latest.yaml
+sed -i "s/minor: .*/minor: $MINOR/g; s/patch: .*/patch: $BUGFIX/g" ".github/workflows//build-latest.yaml"
 
 git commit -a -m "Upgraded the GeoServer instance from version ${GS_VERSION} to ${GS_NEW_VERSION}"
 
