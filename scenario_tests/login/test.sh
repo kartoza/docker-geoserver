26e2299dca80d3c58fab342e1d9c698844d51dd5
Admire <addloe@gmail.com>
2025-05-19 19:01:53 +0200

    fix tests

--- scenario_tests/login/test.sh
+++ scenario_tests/login/test.sh
@@ -14,7 +14,7 @@ if [[ -n "${PRINT_TEST_LOGS}" ]]; then
 fi
 
 
-services=("geoserver" "server" "credentials")
+services=("geoserver" "server" "credentials" "users")
 START_PORT=8081
 
 for i in "${!services[@]}"; do
@@ -32,14 +32,39 @@ for i in "${!services[@]}"; do
     USER="myadmin"
   fi
 
-  sleep 30
-  echo -e "[Unit Test] Test URL availability for: \e[1;31m $service \033[0m"
-  test_url_availability "http://localhost:$PORT/geoserver/rest/about/version.xml" "$PASS" "$USER"
 
-  echo -e "\e[32m ---------------------------------------- \033[0m"
-  echo -e "[Unit Test] Execute test for: \e[1;31m $service \033[0m"
-  ${VERSION} exec -T "$service" /bin/bash /tests/test.sh
+  if [[ "$service" != "users" ]];then
+    echo -e "[Unit Test] Test URL availability for: \e[1;31m $service \033[0m"
+    test_url_availability "http://localhost:$PORT/geoserver/rest/about/version.xml" "$PASS" "$USER"
+    echo -e "\e[32m ---------------------------------------- \033[0m"
+    echo -e "[Unit Test] Execute test for: \e[1;31m $service \033[0m"
+    ${VERSION} exec -T "$service" /bin/bash /tests/test.sh
+  else
+    # Execute tests
+    GEOSERVER_ADMIN_PASSWORD=myawesomegeoserver,mygeoserver,mysample
+    GEOSERVER_ADMIN_USER=foo,myadmin,sample
+    COUNT_GEOSERVER_ADMIN_PASSWORD=$(echo "$GEOSERVER_ADMIN_PASSWORD" | tr ',' '\n' | wc -l)
+    IFS=','
+    read -a geopass <<< "$GEOSERVER_ADMIN_PASSWORD"
+    read -a geouser <<< "$GEOSERVER_ADMIN_USER"
+    services=("users")
+    for i in "${!services[@]}"; do
+      service="${services[$i]}"
+      for ((i = 0; i < ${COUNT_GEOSERVER_ADMIN_PASSWORD}; i++)); do
+              USER="${geouser[$i]}"
+              PASS="${geopass[$i]}"
+              echo -e "[Unit Test] Test URL availability for: \e[1;31m $service with user $USER and pass $PASS \033[0m"
+              test_url_availability http://localhost:$PORT/geoserver/rest/about/version.xml ${PASS} ${USER}
+              echo "Execute test for $service"
+              docker compose exec -T "$service" /bin/bash /tests/test.sh
+      done
+    done
+
+  fi
+
 done
 
-${VERSION} down -v
+
+docker compose down -v
+
 
--- scenario_tests/login/tests/test_login.py
+++ scenario_tests/login/tests/test_login.py
@@ -11,25 +11,41 @@ class TestGeoServerREST(unittest.TestCase):
         self.login_url = f'{self.base_url}/j_spring_security_check'
         self.container_name = environ['CONTAINER_NAME']
 
-        if self.container_name == 'geoserver':
-            self.password = environ['GEOSERVER_ADMIN_PASSWORD']
-            self.username = 'admin'
-        elif self.container_name == 'credentials':
-            self.password = environ['GEOSERVER_ADMIN_PASSWORD']
-            self.username = 'myadmin'
-        elif self.container_name == 'server':
-            self.username = 'admin'
-            with open('/opt/geoserver/data_dir/security/pass.txt', 'r') as file:
-                file_pass = file.read()
-            self.password = file_pass.replace("\n", "")
-        self.session = requests.Session()
-        login_data = {
-            'username': self.username,
-            'password': self.password,
-            'submit': 'Login'
-        }
-        response = self.session.post(self.login_url, data=login_data)
-        self.assertEqual(response.status_code, 200)
+        if self.container_name =='users':
+
+            split_username = environ['GEOSERVER_ADMIN_USER'].split(',')
+            split_password = environ['GEOSERVER_ADMIN_PASSWORD'].split(',')
+            credentials = dict(zip(split_username, split_password))
+            for username, password in credentials.items():
+
+                self.session = requests.Session()
+                login_data = {
+                    'username': username,
+                    'password': password,
+                    'submit': 'Login'
+                }
+                response = self.session.post(self.login_url, data=login_data)
+                self.assertEqual(response.status_code, 200)
+        else:
+            if self.container_name == 'geoserver':
+                self.password = environ['GEOSERVER_ADMIN_PASSWORD']
+                self.username = 'admin'
+            elif self.container_name == 'credentials':
+                self.password = environ['GEOSERVER_ADMIN_PASSWORD']
+                self.username = 'myadmin'
+            elif self.container_name == 'server':
+                self.username = 'admin'
+                with open('/opt/geoserver/data_dir/security/pass.txt', 'r') as file:
+                    file_pass = file.read()
+                self.password = file_pass.replace("\n", "")
+            self.session = requests.Session()
+            login_data = {
+                'username': self.username,
+                'password': self.password,
+                'submit': 'Login'
+            }
+            response = self.session.post(self.login_url, data=login_data)
+            self.assertEqual(response.status_code, 200)
 
     def test_rest_endpoints_accessible(self):
         # Test if the REST endpoints are accessible as a logged user
