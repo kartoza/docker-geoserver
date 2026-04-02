8d8bc2ff9bd733debfdb9bafdcd8114f9ab19709
Admire <addloe@gmail.com>
2026-04-02 19:30:51 +0200

    make entrypoint use functions

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1491,7 +1491,7 @@ run_update_password_hook() {
 }
 
 run_pre_start_hooks() {
-  local hook="${SCRIPT_DIR}/start.sh"
+  local hook="/scripts/start.sh"
 
   if [[ -x "${hook}" ]]; then
     log "Running pre-start hook"
@@ -1524,6 +1524,25 @@ create_setup_directories() {
     create_dir "${d}"
   done
 }
+
+create_entrypoint_directories() {
+  local dirs=(
+    "${GEOSERVER_DATA_DIR}"
+    "${CERT_DIR}"
+    "${FOOTPRINTS_DATA_DIR}"
+    "${FONTS_DIR}"
+    "${GEOWEBCACHE_CACHE_DIR}"
+    "${GEOSERVER_HOME}"
+    "${EXTRA_CONFIG_DIR}"
+    "/docker-entrypoint-geoserver.d"
+  )
+
+  for d in "${dirs[@]}"; do
+    create_dir "${d}"
+  done
+}
+
+
 
 install_geoserver_core() {
   pushd "${CATALINA_HOME}" || exit
@@ -1608,4 +1627,193 @@ set_restrictive_umask() {
 cleanup_resources() {
   rm -rf /tmp/resources
   delete_file "${CATALINA_HOME}/conf/web.xml"
+}
+
+init_runtime_user_vars() {
+  USER_ID="${GEOSERVER_UID:-2000}"
+  GROUP_ID="${GEOSERVER_GID:-2000}"
+  USER_NAME="${USER:-geoserveruser}"
+  GEO_GROUP_NAME="${GROUP_NAME:-geoserverusers}"
+
+  export USER_ID GROUP_ID USER_NAME GEO_GROUP_NAME
+}
+
+ensure_group_exists() {
+  if ! getent group "${GEO_GROUP_NAME}" >/dev/null; then
+    groupadd -r "${GEO_GROUP_NAME}" -g "${GROUP_ID}"
+  fi
+}
+
+ensure_user_exists() {
+  if ! id -u "${USER_NAME}" >/dev/null 2>&1; then
+    useradd \
+      -l \
+      -m \
+      -d "/home/${USER_NAME}" \
+      -u "${USER_ID}" \
+      --gid "${GROUP_ID}" \
+      -s /bin/bash \
+      -G "${GEO_GROUP_NAME}" \
+      "${USER_NAME}"
+  fi
+}
+
+setup_geoserver_users() {
+  init_runtime_user_vars
+  ensure_group_exists
+  ensure_user_exists
+}
+
+rename_geoserver_context_root() {
+  if [[ "${GEOSERVER_CONTEXT_ROOT}" != "geoserver" ]]; then
+    #log "Changing context-root to ${GEOSERVER_CONTEXT_ROOT}"
+    echo "Changing context-root to ${GEOSERVER_CONTEXT_ROOT}"
+    local install_dir
+    install_dir="$(detect_install_dir)"
+
+    if [[ -d "${install_dir}/webapps/geoserver" ]]; then
+      mv "${install_dir}/webapps/geoserver" \
+         "${install_dir}/webapps/${GEOSERVER_CONTEXT_ROOT}"
+    else
+      #log_warn "Context already renamed or first-run skipped"
+      echo "Context already renamed or first-run skipped"
+    fi
+  fi
+}
+
+configure_jvm() {
+  if [[ -z ${GEOSERVER_OPTS} ]];then
+
+  export GEOSERVER_OPTS="-Djava.awt.headless=true -server -Xms${INITIAL_MEMORY} -Xmx${MAXIMUM_MEMORY} \
+       -XX:PerfDataSamplingInterval=500 -Dorg.geotools.referencing.forceXY=true \
+       -XX:SoftRefLRUPolicyMSPerMB=36000   \
+       -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:ParallelGCThreads=20 -XX:ConcGCThreads=5 \
+       -XX:InitiatingHeapOccupancyPercent=${INITIAL_HEAP_OCCUPANCY_PERCENT}  \
+       -Djts.overlay=ng \
+       -Dfile.encoding=${ENCODING} \
+       -Duser.timezone=${TIMEZONE} \
+       -Duser.language=${LANGUAGE} \
+       -Duser.region=${REGION} \
+       -Duser.country=${COUNTRY} \
+       -DENABLE_JSONP=${ENABLE_JSONP} \
+       -DMAX_FILTER_RULES=${MAX_FILTER_RULES} \
+       -DOPTIMIZE_LINE_WIDTH=${OPTIMIZE_LINE_WIDTH} \
+       -DALLOW_ENV_PARAMETRIZATION=${ALLOW_ENV_PARAMETRIZATION} \
+       -Djavax.servlet.request.encoding=${CHARACTER_ENCODING} \
+       -Djavax.servlet.response.encoding=${CHARACTER_ENCODING} \
+       -DCLUSTER_CONFIG_DIR=${CLUSTER_CONFIG_DIR} \
+       -DGEOSERVER_DATA_DIR=${GEOSERVER_DATA_DIR} \
+       -DGEOSERVER_FILEBROWSER_HIDEFS=${GEOSERVER_FILEBROWSER_HIDEFS} \
+       -DGEOSERVER_AUDIT_PATH=${MONITOR_AUDIT_PATH} \
+       -Dorg.geotools.shapefile.datetime=${USE_DATETIME_IN_SHAPEFILE} \
+       -Dorg.geotools.localDateTimeHandling=true \
+       -Dsun.java2d.renderer.useThreadLocal=false \
+       -Dsun.java2d.renderer.pixelsize=8192 -server -XX:NewSize=300m \
+       -Dlog4j.configuration=${CATALINA_HOME}/log4j.properties \
+       --patch-module java.desktop=${CATALINA_HOME}/lib/marlin.jar \
+       -Dsun.java2d.renderer=org.marlin.pisces.PiscesRenderingEngine \
+       -Dsun.java2d.renderer.log=true \
+       -Dgeoserver.login.autocomplete=${LOGIN_STATUS} \
+       -DUPDATE_BUILT_IN_LOGGING_PROFILES=${UPDATE_LOGGING_PROFILES} \
+       -DRELINQUISH_LOG4J_CONTROL=${RELINQUISH_LOG4J_CONTROL} \
+       -DGEOSERVER_CONSOLE_DISABLED=${DISABLE_WEB_INTERFACE} \
+       -DGWC_DISKQUOTA_DISABLED=${DISKQUOTA_DISABLED} \
+       -DGEOSERVER_CSRF_WHITELIST=${CSRF_WHITELIST} \
+       -Dgeoserver.xframe.shouldSetPolicy=${XFRAME_OPTIONS} \
+       -DGEOSERVER_REQUIRE_FILE=${GEOSERVER_REQUIRE_FILE} \
+       -DENTITY_RESOLUTION_ALLOWLIST='"${ENTITY_RESOLUTION_ALLOWLIST}"' \
+       -DGEOSERVER_DISABLE_STATIC_WEB_FILES=${GEOSERVER_DISABLE_STATIC_WEB_FILES} \
+       -DGEOSERVER_DATA_DIR_LOADER_ENABLED=${GEOSERVER_DATA_DIR_LOADER_ENABLED} \
+       -DGEOSERVER_DATA_DIR_LOADER_THREADS=${GEOSERVER_DATA_DIR_LOADER_THREADS} \
+       ${ADDITIONAL_JAVA_STARTUP_OPTIONS} "
+  fi
+  ## Prepare the JVM command line arguments
+  export JAVA_OPTS="${JAVA_OPTS} ${GEOSERVER_OPTS}"
+}
+
+configure_telemetry() {
+  [[ ${TELEMETRY_TRACING_ENABLED} =~ [Tt][Rr][Uu][Ee] ]] && \
+    JAVA_OPTS+=" -javaagent:${OTEL_DIR}/opentelemetry-javaagent.jar"
+
+  [[ ${TELEMETRY_METRICS_ENABLED} =~ [Tt][Rr][Uu][Ee] ]] && \
+    JAVA_OPTS+=" -javaagent:${JMX_DIR}/jmx_prometheus_javaagent.jar=${TELEMETRY_METRICS_PORT}:${JMX_DIR}/config.yaml"
+
+  export JAVA_OPTS
+}
+
+fix_permissions() {
+
+  change_owner_if_needed() {
+    local target="$1"
+    if [[ -e "$target" ]]; then
+      local owner group
+      owner=$(stat -c '%U' "$target")
+      group=$(stat -c '%G' "$target")
+      if [[ "$owner" != "$USER_NAME" || "$group" != "$GEO_GROUP_NAME" ]]; then
+        echo -e "\e[32m [Entrypoint] Changing folder permission for:\033[0m \e[1;31m $target \033[0m"
+        chown -R "$USER_NAME":"$GEO_GROUP_NAME" "$target"
+      fi
+    fi
+  }
+
+  if [[ ${RUN_AS_ROOT} =~ [Ff][Aa][Ll][Ss][Ee] ]]; then
+    dir_ownership=(
+      "${CATALINA_HOME}/bin"
+      "${CATALINA_HOME}/conf"
+      "/home/${USER_NAME}/"
+      "${COMMUNITY_PLUGINS_DIR}"
+      "${STABLE_PLUGINS_DIR}"
+      "${REQUIRED_PLUGINS_DIR}"
+      "${GEOSERVER_HOME}"
+      "/usr/share/fonts/"
+      "${FOOTPRINTS_DATA_DIR}"
+      "${CERT_DIR}"
+      "${FONTS_DIR}"
+      "/scripts/"
+      "${EXTRA_CONFIG_DIR}"
+      "/docker-entrypoint-geoserver.d"
+      "${MONITOR_AUDIT_PATH}"
+    )
+
+    for directory in "${dir_ownership[@]}"; do
+      change_owner_if_needed "$directory"
+    done
+
+    # Special cases
+    change_owner_if_needed "${CLUSTER_CONFIG_DIR}"
+    change_owner_if_needed "${GEOSERVER_DATA_DIR}/logging.xml"
+    change_owner_if_needed "${GEOSERVER_DATA_DIR}/jdbcconfig"
+    change_owner_if_needed "${GEOSERVER_DATA_DIR}/jdbcstore"
+    change_owner_if_needed "${GEOSERVER_LOG_DIR}"
+    change_owner_if_needed "${GEOSERVER_DATA_DIR}/cluster"
+  fi
+
+  # Permissions adjustments
+  chmod o+rw "${CERT_DIR}"
+  echo -e "\e[32m [Entrypoint] Fixing Permissions for :\033[0m \e[1;31m ${GEOSERVER_DATA_DIR} &&  ${GEOWEBCACHE_CACHE_DIR}  \033[0m"
+  gwc_file_perms
+  find "${CATALINA_HOME}/conf/" -type f -exec chmod 400 {} \;
+
+  # Sample data ownership fix
+
+  if [[ ${SAMPLE_DATA} =~ [Tt][Rr][Uu][Ee] ]]; then
+      change_owner_if_needed "${GEOSERVER_DATA_DIR}"
+  fi
+}
+
+start_geoserver_service() {
+if [[ ${RUN_AS_ROOT} =~ [Ff][Aa][Ll][Ss][Ee] ]];then
+  if [[ -f ${GEOSERVER_HOME}/start.jar ]]; then
+    exec gosu "${USER_NAME}" "${GEOSERVER_HOME}"/bin/startup.sh
+  else
+    exec gosu "${USER_NAME}" /usr/local/tomcat/bin/catalina.sh run
+  fi
+else
+  if [[ -f ${GEOSERVER_HOME}/start.jar ]]; then
+    exec  "${GEOSERVER_HOME}"/bin/startup.sh
+  else
+    exec  /usr/local/tomcat/bin/catalina.sh run
+  fi
+fi
+
 }
