f7db47aaa33970ab45d6017965de83636a84522e
mazano <kartozahubnotifications@gmail.com>
2026-08-19 12:55:48 +0200

    Update community generation

--- build_data/plugins/extensions.sh
+++ build_data/plugins/extensions.sh
@@ -4,12 +4,14 @@ set -eux
 
 
 GS_VERSION_LATEST="${GS_VERSION%.*}".x
+GS_VERSION_COMMUNITY="${GS_VERSION_COMMUNITY:-${GS_VERSION%.*}.0}"
+export GS_VERSION_COMMUNITY
 
 
 python3 /work/stable_plugins.py ${GS_VERSION} https://sourceforge.net/projects/geoserver/files/GeoServer
 
 python3 /work/community_plugins.py \
   "${GS_VERSION_LATEST}" \
-  "${COMMUNITY_EXTENSION_PLUGIN_BASE_URL}"
-
+  "${COMMUNITY_EXTENSION_PLUGIN_BASE_URL}" \
+  "${GS_VERSION_COMMUNITY}"
 
